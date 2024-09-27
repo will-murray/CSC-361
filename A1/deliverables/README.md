8cf026f5323b001c17d2736cd6f57d61b50d9dd9
Will Murray <williammurray29@gmail.com>
2024-09-27 12:40:12 -0700

    added deliverables

---
+++ A1/deliverables/README.md
@@ -0,0 +1,5 @@
+# Running this code
+
+To run Webtester.py use the following command:
+
+```python3 WebTester.py <uri>```
--- A1/test.sh
+++ A1/test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # test cases
 urls=(
-  #"https://www.uvic.ca"
+  "https://www.uvic.ca"
   "http://www.uvic.ca"
   "http://www.uvic.ca/tools/"
   "http://chatgpt.com"
-  
-  #"google.com"
-
+  "google.com"
+  "https://www.bing.com/"
+  "http://www.bing.com/"
+  "cloudflare.com"
+  "not a uri"
+  "docs.engr.uvic.ca/docs"
 )
 
 

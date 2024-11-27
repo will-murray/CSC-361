7234210e652b20d0d482d4588de2420ecd8a44ae
William Murray <williammurray29@gmail.com>
2024-11-27 23:55:03 +0000

    belligerent OOP solution >:)

--- A3/p3.py
+++ A3/p3.py
@@ -11,26 +11,18 @@ class IP_Datagram():
     def __init__(self,buffer,incl_len,ts_sec, ts_usec):
         self.timestamp = reference_datetime + timedelta(seconds=ts_sec,microseconds=ts_usec)
         self.ethernet_header = buffer[:14]
-        self.header = IP_Header(buffer)
-        self.payload = buffer[14 + self.header.header_length:] #ethernet header length + IP header length
+        self.header = IP_Header(buffer[14:34])
 
-        if self.header.proto == 17: # UDP
-            udp_header = self.payload[:8]  # IP header is 20 bytes, UDP header is 8 bytes
-            self.udp_fields = struct.unpack("!HHHH", udp_header)
-            self.udp_src_port = self.udp_fields[0]
+        raw_payload = buffer[14 + self.header.header_length:] #ethernet header length + IP header length
+       
+        if self.header.proto == 1: # ICMP
+            self.payload = ICMP_Message(raw_payload)
+
+        elif self.header.proto == 17: # UDP
+            self.payload = UDP_Message(raw_payload)
         
-        elif self.header.proto == 6: # TCP
-            pass
-
-        else: # ICMP
-            self.icmp_header = self.payload[:8]
-            self.icmp_fields = struct.unpack("!BBHHH", self.icmp_header) 
-            print(self.icmp_fields)
-            # self.icmp_type = self.icmp_fields[0]
-            # self.icmp_id = self.icmp_fields[2]
-            # if self.icmp_type == 11:s
-            #     self.OG_header = IP_Header(self.payload[8:28])
-
+        else: # proto == TCP
+            self.payload = TCP_Message(raw_payload)
     
 
 
@@ -38,8 +30,8 @@ class IP_Datagram():
 
 class IP_Header():
     def __init__(self, buffer):
-        ip_header = buffer[14:34]
-        ip_fields = struct.unpack("!BBHHHBBH4s4s", ip_header)
+        ip_fields = struct.unpack("!BBHHHBBH4s4s", buffer)
+        self.id = ip_fields[3]
         self.ttl = ip_fields[5]
         self.proto = ip_fields[6]
         self.source = socket.inet_ntoa(ip_fields[8])
@@ -49,10 +41,36 @@ class IP_Header():
         self.ihl = ip_fields[0] & 0x0F  # Extract the lower 4 bits from the first byte (IHL)
         self.header_length = self.ihl * 4  # Multiply by 4 to get the actual length in bytes
 
+
+
     
 
     def __str__(self):
-        print(f"{self.source} -> {self.dest}")
+        return f"id : {self.id} | proto : {self.proto}"
+
+class ICMP_Message():
+    def __init__(self,buffer):
+        self.icmp_header = buffer[:8]
+        self.icmp_fields = struct.unpack("!BBHHH", self.icmp_header) 
+        self.icmp_type = self.icmp_fields[0]
+        self.icmp_id = self.icmp_fields[2]
+        self.OG_header = IP_Header(buffer[8:28])
+
+class UDP_Message():
+
+    def __init__(self,buffer):
+        udp_header = buffer[:8]  # IP header is 20 bytes, UDP header is 8 bytes
+        udp_fields = struct.unpack("!HHHH", udp_header)
+        self.udp_src_port = udp_fields[0]
+        self.udp_dest_port = udp_fields[1]
+
+class TCP_Message():
+
+    def __init__(self,buffer):
+        self.src_port, self.dest_port, self.seq, self.ack, self.offset_reserved_flags, self.window_size = struct.unpack('!HHLLHH', buffer[:16])
+        self.offset = (self.offset_reserved_flags >> 12) * 4 
+        self.flags = self.offset_reserved_flags & 0x01FF  # Last 9 bits are the flags
+        
 
 def analyze_traceroute(file_path):
     # Open the PCAP file
@@ -60,9 +78,6 @@ def analyze_traceroute(file_path):
         # Read and parse the PCAP global header (24 bytes)
         global_header = f.read(24)
         
-
-        
-       
         L = [] #set of datagrams
         # Iterate over the packet headers and data
         while True:
@@ -84,6 +99,10 @@ def analyze_traceroute(file_path):
 
 # Example usage
 L = analyze_traceroute(sys.argv[1])
+
+for l in L:
+    if l.header.proto == 1: #ICMP message
+        pass
 
 
 
